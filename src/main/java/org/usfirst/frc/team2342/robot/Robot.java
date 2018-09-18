9fe453be420097fbf905315c8100751b64813cef
denver-blake <denver.blake26@gmail.com>
2018-09-17 20:23:26 -0400

    Upgrade phoenix2018 Eclipse to VSCode

---
+++ src/main/java/org/usfirst/frc/team2342/robot/Robot.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team2342.robot;
+
+import edu.wpi.first.wpilibj.SampleRobot;
+
+/**
+ * This is a demo program showing how to use Mecanum control with the RobotDrive
+ * class.
+ */
+public class Robot extends SampleRobot {
+
+	public Robot() {
+		
+	}
+
+	@Override
+	public void operatorControl() {
+		
+	}
+	
+	@Override
+	public void autonomous() {
+		
+	}
+	
+	@Override
+	public void test() {
+		
+	}
+}
